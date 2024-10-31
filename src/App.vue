d128b2b99cb38fd5ff66060d1fb9b9e2d19ac65f
dyb-dev <2737993954@qq.com>
2024-10-31 23:55:06 +0800

    🐞 fix: 修复并优化重定向逻辑

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
  * @Author: dyb-dev
  * @Date: 2024-10-05 20:10:11
  * @LastEditors: dyb-dev
- * @LastEditTime: 2024-10-24 00:58:54
+ * @LastEditTime: 2024-10-31 23:30:28
  * @FilePath: /mp-wx-dyb-dev/src/App.vue
  * @Description: 程序全局组件
 -->
@@ -10,65 +10,27 @@
 <script setup lang="ts">
 import { onLaunch, onShow, onHide } from "@dcloudio/uni-app"
 
-import { useUserInfoStore } from "@/stores"
-import { navigateToLogin, navigateToPage } from "@/utils"
-
-import type { INavigateToPageOptions } from "@/utils"
+// import { useUserInfoStore } from "@/stores"
 
 /** HOOKS: 用户信息商店 */
-const { userInfoStoreState } = useUserInfoStore()
-
-/** 小程序启动时的查询参数 */
-interface ILaunchQuery {
-    /** `VITE_USE_LAUNCH_PAGE`为`true`时有效 目标页面路径 默认: 首页 */
-    targetPath?: NavigateToOptions["url"]
-    /** 其他相关参数 */
-    [key: string]: any
-}
+// const { login } = useUserInfoStore()
 
 /** LIFECYCLE: 小程序首次进入 */
-onLaunch(async options => {
+onLaunch(options => {
 
     console.log("项目信息:", __PROJECT_INFO__)
     console.log("小程序首次进入:", options)
 
-    const { VITE_USE_LAUNCH_PAGE, VITE_LOGIN_PATH, VITE_HOME_PATH, VITE_LAUNCH_PATH } = import.meta.env
+    // const { VITE_USE_LAUNCH_PAGE } = import.meta.env
 
-    // 如果使用了启动页，则需要手动跳转到目标页面
-    if (VITE_USE_LAUNCH_PAGE === "true") {
+    // 如果使用启动页，则登录在启动页中的 `onLoad` 进行，因此这里不再登录，避免重复登录
+    // if (VITE_USE_LAUNCH_PAGE === "true") {
 
-        // 获取小程序启动时的查询参数
-        const _query = <ILaunchQuery>options?.query || {}
+    //     return
 
-        const { targetPath = VITE_HOME_PATH, ...otherQuery } = _query
+    // }
 
-        // 如果 targetPath 为启动页路径，则修改为首页路径
-        const _targetPath = `/${targetPath === VITE_LAUNCH_PATH ? VITE_HOME_PATH : targetPath}` as NavigateToOptions["url"]
-
-        // 跳转参数
-        const _options: INavigateToPageOptions = {
-            path: _targetPath,
-            query: otherQuery,
-            method: "reLaunch",
-            findBackDelta: false
-        }
-
-        // 如果未登录，则跳转到登录页，登录成功后再在重定向到目标页
-        if (!userInfoStoreState.isLogin) {
-
-            navigateToLogin({
-                ..._options,
-                redirectPath: _options.path,
-                path: `/${VITE_LOGIN_PATH}` as NavigateToOptions["url"]
-            })
-
-            return
-
-        }
-
-        navigateToPage(_options)
-
-    }
+    // login()
 
 })
 
