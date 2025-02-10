1a9d906d35772d1a9863334353d1fea5aa5e36d6
dyb-dev <2737993954@qq.com>
2025-02-10 20:19:05 +0800

    🦄 refactor: 优化代码结构

--- .stylelintrc.mjs
+++ .stylelintrc.mjs
@@ -2,8 +2,8 @@
  * @Author: dyb-dev
  * @Date: 2024-03-20 16:17:30
  * @LastEditors: dyb-dev
- * @LastEditTime: 2024-10-05 20:01:38
- * @FilePath: /uniapp-mp-wx-template/.stylelintrc.mjs
+ * @LastEditTime: 2025-02-10 19:49:21
+ * @FilePath: /mp-wx-dyb-dev/.stylelintrc.mjs
  * @Description: stylelint配置文件 注意：每次配置文件的更改，建议重启一下vscode，否则可能不会生效
  */
 
@@ -11,6 +11,8 @@ export default {
     extends: ["@dyb-dev/stylelint-config"],
     rules: {
         // 忽略 `rpx` 单位的报错
-        "unit-no-unknown": [true, { ignoreUnits: ["rpx"] }]
+        "unit-no-unknown": [true, { ignoreUnits: ["rpx"] }],
+        // 忽略 `rpx` 单位的报错
+        "declaration-property-value-no-unknown": null
     }
 }
